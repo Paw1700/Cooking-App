7ba7454841ddbacde62360f9183213264ff8a743
Paw1700 <uryckip@gmail.com>
2024-12-13 14:50:56 +0100

    ADD - Recipe Page - Edit HTML Skeleton

--- src/app/pages/recipe/components/ingredients_box/ingredients_box.component.ts
+++ src/app/pages/recipe/components/ingredients_box/ingredients_box.component.ts
@@ -1,14 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { ContentBox } from "../../../../components/content_box.component";
+import { AddButton } from "../../../../components/add_button/add_button.component";
 
 @Component({
     selector: 'ingredients_box',
     templateUrl: './ingredients_box.component.html',
     styleUrl: './ingredients_box.component.scss',
     imports: [
-        ContentBox
+        ContentBox,
+        AddButton
     ]
 })
 export class IngredientsBox {
+    @Input() edit_mode = false
     test_list: boolean[] = Array(10).fill(false);
 }
--- src/app/pages/recipe/components/steps_box/steps.component.ts
+++ src/app/pages/recipe/components/steps_box/steps.component.ts
@@ -1,14 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { ContentBox } from "../../../../components/content_box.component";
+import { AddButton } from "../../../../components/add_button/add_button.component";
 
 @Component({
     selector: 'steps_box',
     templateUrl: './steps.component.html',
     styleUrl: './steps.component.scss',
     imports: [
-        ContentBox
+        ContentBox,
+        AddButton
     ]
 })
 export class StepsBox {
+    @Input() edit_mode = true
     test_list: boolean[] = Array(10).fill(false)
 }
