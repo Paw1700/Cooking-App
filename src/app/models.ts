cdbd1c77708740394e61d752e45b43919cc79a64
Paw1700 <uryckip@gmail.com>
2024-12-16 12:35:48 +0100

    ADD - In-App Models

--- src/app/models.ts
+++ src/app/models.ts
@@ -1,1 +1,33 @@
 export type AppLocations = 'home' | 'recipes_list' | 'recipe'
+
+export interface Category {
+    id: string,
+    name: string
+}
+
+export interface SubCategory extends Category {
+    category_id: string
+}
+
+export interface Recipe {
+    id: string,
+    sub_category_id: string,
+    name: string,
+    photo_id: string,
+    ingredients: Ingredient[],
+    steps: Step[]
+}
+
+export interface Ingredient {
+    name: string,
+    amount: string
+}
+
+export interface Step {
+    description: string
+}
+
+export interface Photo {
+    id: string,
+    photo_string: string
+} 
